19b115150befec81ca1cdc621402fa6887c63b8e
Jai Jalah Kumar <jay.jk@hotmail.com>
2021-03-18 23:00:15 +0000

    Get all profiles & profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,7 +7,7 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 // @route    GET api/profile/me
-// @desc     Get current users profile
+// @desc     Get current user's profile
 // @access   Private
 router.get('/me', auth, async (req, res) => {
   try {
@@ -26,7 +26,7 @@ router.get('/me', auth, async (req, res) => {
 });
 
 // @route    POST api/profile/
-// @desc     Create or udpate user profile
+// @desc     Create or update user profile
 // @access   Private
 router.post('/', [ auth, [
   check('status', 'Status is required')
@@ -106,6 +106,39 @@ async (req, res) => {
 }
 );
 
+// @route    GET api/profile/
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async(req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
+router.get('/user/:user_id', async(req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user',
+    ['name', 'avatar']);
+
+    if (!profile)
+      return res.status(400).json({ msg: 'Profile not found' }
+  );
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' })
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
